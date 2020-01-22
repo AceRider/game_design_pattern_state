372bafd2d892b791b28ddb3cbd81efd2e6058a0a
aecio <aecio.jorge@gmail.com>
2020-01-22 19:07:33 -0400

    state part3

--- Assets/GameEnvironment.cs
+++ Assets/GameEnvironment.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public sealed class GameEnvironment 
 {
@@ -16,6 +17,7 @@ public sealed class GameEnvironment
             {
                 instance = new GameEnvironment();
                 instance.Checkpoints.AddRange(GameObject.FindGameObjectsWithTag("Checkpoint"));
+                instance.checkpoints = instance.checkpoints.OrderBy(waypoint => waypoint.name).ToList();
             }
             return instance;
         }
